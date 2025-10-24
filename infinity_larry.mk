440d38211992f6f474a307791a7963a0d25e01c7
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-10-24 11:17:02 +0530

    Add InfinityX configuration to infinity_larry.mk
    Added InfinityX flags and configuration settings.

--- infinity_larry.mk
+++ infinity_larry.mk
@@ -15,6 +15,19 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from larry device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# InfinityX Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := false
+TARGET_SHIPS_FULL_GAPPS := false
+TARGET_BUILD_GOOGLE_TELEPHONY := false
+TARGET_SUPPORTS_BLUR := true
+USE_MOTO_CALCULATOR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_VIMUSIC := true
+INFINITY_MAINTAINER := Ansh Agnihotri
+INFINITY_BUILD_TYPE := UNOFFICIAL
+
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_larry
 PRODUCT_DEVICE := larry
