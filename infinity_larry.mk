895039ab25cf37d395eb21057387915108e2ce5d
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-10-27 12:16:26 +0530

    Update infinity_larry.mk

--- infinity_larry.mk
+++ infinity_larry.mk
@@ -17,7 +17,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # InfinityX Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GAPPS := false
+WITH_GAPPS := true
 INFINITY_MAINTAINER := Ansh Agnihotri
 INFINITY_BUILD_TYPE := UNOFFICIAL
 
