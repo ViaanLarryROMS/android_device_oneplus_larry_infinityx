e7c47b4b4337729e2274ca805fef119004a7865b
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-10-28 11:23:46 +0530

    Update infinity_larry.mk

--- infinity_larry.mk
+++ infinity_larry.mk
@@ -18,9 +18,10 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # InfinityX Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS := false
-INFINITY_MAINTAINER := Ansh Agnihotri
+INFINITY_MAINTAINER := Ansh_Agnihotri
 INFINITY_BUILD_TYPE := UNOFFICIAL
-
+TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := infinity_larry
