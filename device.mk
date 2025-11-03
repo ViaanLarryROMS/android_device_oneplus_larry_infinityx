97e2bf8f596393a877bdceb3f49ddf84076c35a0
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-11-03 18:45:32 +0530

    Add ViPER4AndroidFX configuration to device.mk

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, device/oneplus/sm6375-common/sm6375.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# ViPER4AndroidFX
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_qrd.xml \
