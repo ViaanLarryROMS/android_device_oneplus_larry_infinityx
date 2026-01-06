1157f558b7975945449067c3d0a3845275f5a87f
C R E S T <shrishtiiii96@gmail.com>
2026-01-06 17:30:38 +0530

    Fix LiveDisplay HAL configuration setting

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ PRODUCT_PACKAGES += \
     init.device.rc
 
 # LiveDisplay
-$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
+$(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # NFC
 PRODUCT_COPY_FILES += \
